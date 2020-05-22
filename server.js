ec3413d8625a4f14e4a995cd202189732a53a8eb
lauren garelick <laurengarelick@gmail.com>
2020-05-22 02:13:12 -0700

    fixed some of the js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
+const express = require('express'); 
+const app = express();               //express().use()..... too long
+require('dotenv').config(); 
 const path = require('path');
 const db = require('./db/db.json');
 const fs = require('fs');
-//express().use()..... too long
+
+//whatever is in the environment variable PORT, or 3000 if there's nothing there
 const PORT = process.env.PORT || 3000;
-app.use(express.urlencoded({extended:true}));
+
 app.use(express.json());
 app.use(express.static('public'));
 app.get('/notes', (req,res)=>{
@@ -19,13 +20,16 @@ app.post('/api/notes', (req,res)=>{
     db.push(req.body)
     fs.writeFile('./db/db.json', JSON.stringify(db), (err)=> res.json(err||"success!") )
 })
-app.get("/members/:password", (req,res)=>{
-    if(req.params.password === process.env.SECRET_PASSWORD){
-        res.sendFile(path.join(__dirname, './public/members.html'))
-    }else{
-        res.json('YOU CANT HAVE NONE!')
-    }
+app.delete('/api/notes/:id', (req, res)=> {
+    res.push('db\db.json')
 })
+// app.get("/members/:password", (req,res)=>{
+//     if(req.params.password === process.env.SECRET_PASSWORD){
+//         res.sendFile(path.join(__dirname, './public/members.html'))
+//     }else{
+//         res.json('YOU CANT HAVE NONE!')
+//     }
+// })
 
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, './public/index.html'))
